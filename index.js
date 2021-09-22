257c4fbcdb1ea284f0b5928d65991e2b6f1a7162
Navi Kaur <navdeep9520@gmail.com>
2021-09-22 10:01:22 -0700

    commit tree 2cf831e5ef6da742359dcc1f59c3e2044add721d

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+   let num1 = 2
+   let num2 = 31
+  const multiply = num1 * num2
+ 
+  Math.floor(Math.random() * 10)
+
+let random = Math.floor(Math.random() * 10)
+
+let num3 = 8
+let num4 = 2
+const mod = num3 / num4
+
+const max = Math.max(1, 2, 3, 4, 20);
